a35e14f31dce5c37fb036aef845015d2f4358b6b
Burrath <vincenzobonaccorso96@gmail.com>
2022-03-23 16:17:55 +0100

    added tests, first version completed

--- contracts/BetaKreativ.sol
+++ contracts/BetaKreativ.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract BetaKreativ is ERC20 {
-    constructor() ERC20("Beta Kreativ", "KREATIV") {}
+contract BetaKreativ is ERC20PresetMinterPauser {
+
+    uint256 maximumTokenAmount = 200000000 ether;
+    uint256 startingTokenAmount = 100000000 ether;
+
+    constructor() ERC20PresetMinterPauser("Beta Kreativ", "KREATIV") {
+        _mint(msg.sender, startingTokenAmount);
+    }
+
+    /**
+     * @dev See {ERC20-_mint}.
+     */
+    function _mint(address account, uint256 amount) internal virtual override {
+        require(ERC20.totalSupply() + amount <= maximumTokenAmount, "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
+    }
 }
