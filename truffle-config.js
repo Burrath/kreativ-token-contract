c5c32d43462c2c3db467900c5413153127729e07
Burrath <vincenzobonaccorso96@gmail.com>
2022-03-26 11:58:01 +0100

    deploy and verify configurations

--- truffle-config.js
+++ truffle-config.js
@@ -20,11 +20,17 @@
 
 require("dotenv").config();
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const mnemonic = process.env.WALLET_MNEMONIC;
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const mnemonic = process.env.WALLET_MNEMONIC;
 
 module.exports = {
+  // To verify your contract run
+  // truffle run verify <ContractName> --network <network>
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_APY_KEY,
+  },
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
@@ -36,12 +42,6 @@ module.exports = {
    */
 
   networks: {
-    // To verify your contract run
-    // truffle run verify <ContractName> --network <network>
-    // plugins: ["truffle-plugin-verify"],
-    // api_keys: {
-    //   etherscan: process.env.ETHERSCAN_APY_KEY,
-    // },
     //
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -79,6 +79,12 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, process.env.ETHEREUM_MAINNET_RPC);
+      },
+      network_id: 4,
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
